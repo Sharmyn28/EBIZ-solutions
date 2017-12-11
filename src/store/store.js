7d6afcc23cd87272b08816bb50f9a4608df4bb47
gabiprds93 <gabiprds.93@gmail.com>
2017-12-11 16:04:43 -0500

    cambio teachers

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import createStore from 'redux-zero';
 import students from "./Students";
 import registrationCourses from "./RegistrationCourses";
-import teacher from "./teachers";
+import teacher from "./Teachers";
 import course from "./Courses";
 
 const initialState = 
