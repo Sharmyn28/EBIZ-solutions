d9e97037d9f9c3866ae1d13ef6d774134157f7ba
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-12-11 15:10:46 -0500

    promedio

--- src/components/Professor/TableMarks.js
+++ src/components/Professor/TableMarks.js
@@ -1,32 +1,30 @@
 import React from 'react';
 import { Grid, Row, Col, Button, Table } from 'react-bootstrap';
 import SideBar from '../SideBar';
+import registrationCourses from '../../store/RegistrationCourses';
+import { GrandTotal } from "../../actions/actions";
 
-const StudentRecord = ({name, mark1, mark2}) => {
+const StudentRecord = ({ name, mark1, mark2 }) => {
     return (
         <tr>
             <td></td>
             <td>{name}</td>
             <td>{mark1}</td>
             <td>{mark2}</td>
-            <td></td>
+            <td>{GrandTotal(mark1, mark2)}</td>
         </tr>
     )
 }
 
 const TableMarks = () => {
-    const classMarks = bio.map((item) => {
+    const classMarks = registrationCourses.map((e, index) => {
         return (
-            item.colaborativos.map((e, index) => {
-                return (
-                    <StudentRecord
-                        key={index}
-                        name={e.idStudent}
-                        mark1={e.mark1}
-                        mark2={e.mark2}
-                    />
-                )
-            })
+            <StudentRecord
+                key={index}
+                name={e.idStudent}
+                mark1={e.mark1}
+                mark2={e.mark2}
+            />
         )
     })
     return (
@@ -40,7 +38,7 @@ const TableMarks = () => {
                 </tr>
             </thead>
             <tbody>
-                
+                {classMarks}
             </tbody>
         </Table>
     )
