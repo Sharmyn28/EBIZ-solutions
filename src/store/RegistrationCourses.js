2018330c8eb3f17e8547d6353a1159d18a8343f6
gabiprds93 <gabiprds.93@gmail.com>
2017-12-11 14:17:29 -0500

    Notas

--- src/store/RegistrationCourses.js
+++ src/store/RegistrationCourses.js
@@ -4,24 +4,28 @@ export const registrationCourses =
         idRegistration: "r00001",
         idStudent: "s00001",
         idCourse: "c00001",
-        mark: 10,
+        mark1: 10,
+        mark2: 12,        
     },
     {
         idRegistration: "r00002",
         idStudent: "s00002",
         idCourse: "c00002",
-        mark: 12,
+        mark1: 11,
+        mark2: 13,
     },
     {
         idRegistration: "r00003",
         idStudent: "s00003",
         idCourse: "c00003",
-        mark: 14,
+        mark1: 14,
+        mark2: 18,
     },
     {
         idRegistration: "r00001",
         idStudent: "s00004",
         idCourse: "c00004",
-        mark: 18,
+        mark1: 15,
+        mark2: 16,
     }
 ]
