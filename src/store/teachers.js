987f7ff4c7c7ebe057d6f15ee477cee88a6987f5
evecarina <carina120794@gmail.com>
2017-12-11 13:36:45 -0500

    objeto json , profesor y cursos

---
+++ src/store/teachers.js
@@ -0,0 +1,23 @@
+const teachers=
+[
+    {
+        idTeacher:t00001,
+        firstName:"Luz",
+        LastName:"Rodriguez rodriguez"    
+    },
+      {
+        idTeacher:t00002,
+        firstName:"Raúl",
+        LastName:"Perez Sarmiento"    
+    },
+      {
+        idTeacher:t00003,
+        firstName:"Sofia",
+        Last_name:"Palomino Guevara"    
+    },
+     {
+        idTeacher:t00004,
+        firstName:"Jesús",
+        LastName:"Altamirano Flores"    
+    }
+]
