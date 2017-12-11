747f0fe48b1926112a5fd75e23d05c3ee3326f96
evecarina <carina120794@gmail.com>
2017-12-11 14:11:11 -0500

    poniendo todo comillas a los id

--- src/store/teachers.js
+++ src/store/teachers.js
@@ -1,23 +1,29 @@
 const teachers=
 [
     {
-        idTeacher:t00001,
+        idTeacher:"t00001",
         firstName:"Luz",
-        lastName:"Rodriguez rodriguez"    
+        lastName:"Rodriguez rodriguez",
+        password:"t00001" 
     },
       {
-        idTeacher:t00002,
+        idTeacher:"t00002",
         firstName:"Raúl",
-        lastName:"Perez Sarmiento"    
+        lastName:"Perez Sarmiento",
+        password:"t00002" 
+ 
     },
       {
-        idTeacher:t00003,
+        idTeacher:"t00003",
         firstName:"Sofia",
-        last_name:"Palomino Guevara"    
+        last_name:"Palomino Guevara" , 
+        password:"t00003" 
+ 
     },
      {
-        idTeacher:t00004,
+        idTeacher:"t00004",
         firstName:"Jesús",
-        lastName:"Altamirano Flores"    
+        lastName:"Altamirano Flores",
+        password:"t00004"  
     }
 ]
