049118d48cd5f7acfe549c82b288361342729861
gabiprds93 <gabiprds.93@gmail.com>
2017-12-11 13:34:07 -0500

    Infor estudiantes y registro

---
+++ src/store/RegistrationCourses.js
@@ -0,0 +1,27 @@
+const registrationCourses = 
+[
+    {
+        idRegistration: r00001,
+        idStudent: s00001,
+        idCourse: c00001,
+        mark: 10,
+    },
+    {
+        idRegistration: r00002,
+        idStudent: s00002,
+        idCourse: c00002,
+        mark: 12,
+    },
+    {
+        idRegistration: r00003,
+        idStudent: s00003,
+        idCourse: c00003,
+        mark: 14,
+    },
+    {
+        idRegistration: r00001,
+        idStudent: s00004,
+        idCourse: c00004,
+        mark: 18,
+    }
+]
