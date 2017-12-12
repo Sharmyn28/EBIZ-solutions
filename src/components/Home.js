8e408328f7567a315689a0cfd29bc97c1c17b64f
evecarina <carina120794@gmail.com>
2017-12-12 13:09:39 -0500

    modificacion routeo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import '../styles/css/home.css'
+import {NavLink} from 'react-router-dom'
+
+import {  Row, Col } from 'react-bootstrap';
 
 const Home = () => {
     return(
-        <div>
-            Home
-
-            <Button><NavLink to={'/login'}> Profesor</NavLink></Button>
+        <div className="container">
+        <Row className="cont-home" md={6}>
+        <Col md={6}>
+            <Button><NavLink to={'/login'}>Profesor</NavLink></Button>
             <Button>Alumno</Button>
-            <Button>/Familia</Button>
+            <Button>Administrador</Button>
+        </Col>
+        </Row>
         </div>
     )
 }
