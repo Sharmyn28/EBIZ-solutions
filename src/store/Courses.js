dbdb5e34b24f86cf937d762c3c2208943222798a
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-12-12 04:07:00 -0500

    adding list

--- src/store/Courses.js
+++ src/store/Courses.js
@@ -1,25 +1,30 @@
 const courses =
-[
-    {
-        idCourse:"c00001",
-        idTeacher:"t000001",
-        nameCourse:"Introdución a la ingeniería de sistemas"
-    },
-    {
-        idCourse:"c00002",
-        idTeacher:"t00002",
-        nameCourse:"Tésis"
-    },
-    {
-        idCourse:"c00003",
-        idTeacher:"t00002",
-        nameCourse:"Matemática Discreta"
-    },
-    {
-        idCourse:"c00004",
-        idTeacher:"t00003",
-        nameCourse:"Algoritmos"
-    }
-]
+    [
+        {
+            idCourse: "c00001",
+            idTeacher: "t000001",
+            nameCourse: "Introdución a la ingeniería de sistemas"
+        },
+        {
+            idCourse: "c00002",
+            idTeacher: "t00002",
+            nameCourse: "Tésis"
+        },
+        {
+            idCourse: "c00003",
+            idTeacher: "t00002",
+            nameCourse: "Matemática Discreta"
+        },
+        {
+            idCourse: "c00004",
+            idTeacher: "t00003",
+            nameCourse: "Algoritmos"
+        },
+        {
+            idCourse: "c00005",
+            idTeacher: "t00003",
+            nameCourse: "Introduccion a la logica"
+        }
+    ]
 
 export default courses;
