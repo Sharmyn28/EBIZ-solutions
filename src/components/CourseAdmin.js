53840cb12712121058cc8855d73a9c6c4899f294
evecarina <carina120794@gmail.com>
2017-12-12 12:01:22 -0500

    CourseAdmin maquetando

--- src/components/CourseAdmin.js
+++ src/components/CourseAdmin.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Row, Col, Button, Table, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Grid, Row, Col, Button, Table, ListGroup, ListGroupItem,NavItem,Nav,Accordion,Panel} from 'react-bootstrap';
 import SideBar from './SideBar';
 import TableMarks from "./Professor/TableMarks";
 import TableInformation from "./Professor/Info";
@@ -8,25 +8,23 @@ import { filterCourses } from '../actions/actions';
 import { NavLink, Redirect } from 'react-router-dom';
 import { getTeacherStudents } from "../actions/actions";
 
-const CourseAdmin = ({ successLogin }) => {
+const CourseAdmin = ({ successLogin }) => { 
     let courses = filterCourses();
     const listCourses = courses.map((course, index) => {
         return (
-            <ListGroupItem key={index}><NavLink to={"./professor"} onClick={() => getTeacherStudents(course.idCourse)}>{course.nameCourse}</NavLink></ListGroupItem>
+            <ListGroupItem key={index}>{course.nameCourse}</ListGroupItem>
         )
     });
-    //console.log("courses", courses)
-    //console.log("listCourses", listCourses)
     return (
         <Grid fluid>
             {
                 !successLogin && <Redirect to="/login" />
             }
             <Row>
-                <SideBar />
+                <SideBar/>
                 <Col lg={8} md={8}>
                     <h3>Bienvenido!</h3>
-                    <p>Sus cursos a dictar son: </p>
+                    <p>Asigne un curso a un Profesor: </p>
                     <Col lgOffset={4} mdOffset={4} lg={4} md={4}>
                         <ListGroup>
                             {listCourses}
