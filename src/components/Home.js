525395058c58abf17eec3c7a0996eaf4cbe58d54
evecarina <carina120794@gmail.com>
2017-12-12 14:40:47 -0500

    completando el home, con el de gaby

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import '../styles/css/home.css';
 import {NavLink} from 'react-router-dom';
-import { Button , Row, Col } from 'react-bootstrap'
-
+import { Button , Row, Col } from 'react-bootstrap';
+import { saveRol } from '../actions/actions'
 const Home = () => {
     return(
         <div className="container container-home">
@@ -11,9 +11,9 @@ const Home = () => {
         <img width="20%" src="https://upload.wikimedia.org/wikipedia/commons/0/05/Logosimbolo_Universidad_de_La_Salle.png"/>
         </Col>
         <Col md={12} xs={12}>
-            <Button><NavLink to={'/login'}>Profesor</NavLink></Button>
+            <Button><NavLink to={'/login'} id='teacher' onClick={e => saveRol(e.target.id)}>Profesor</NavLink></Button>
             <Button><NavLink to={'/home'}>Alumno</NavLink></Button>
-            <Button><NavLink to={'/login'}>Admin</NavLink></Button>
+            <Button><NavLink to={'/login'} id='admin'  onClick={e => saveRol(e.target.id)}>Admin</NavLink></Button>
         </Col>
         </Row>
         </div>
