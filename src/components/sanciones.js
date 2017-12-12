c650e9b8d9c39d1caa16aca15fe251697671e05f
evecarina <carina120794@gmail.com>
2017-12-12 09:50:32 -0500

    estilos sanciones

--- src/components/sanciones.js
+++ src/components/sanciones.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import SideBar from './SideBar'
+import SideBar from './SideBar';
+import '../styles/css/sanciones.css';
 import { Grid, Row, Col, Button, Table } from 'react-bootstrap';
-
-
 const Sanciones = () => {
     return(
         <div>
             <SideBar/>
-            <Row className="container">
-                <Col md={6}>
-                    <p>Sanciones</p>
+            <Row className="container-sanciones">
+                <Col className="sanciones-img" md={6}>
+                <img  width="30%" src="https://upload.wikimedia.org/wikipedia/commons/0/05/Logosimbolo_Universidad_de_La_Salle.png"/>
                 </Col>
-                <Col md={6}>
-                <img width="50%" src="https://www.ucsm.edu.pe/pucsm/wp-content/uploads/2016/06/escudo_retina.png"/>
-                </Col>
-                <Col md={6}>
+                <Col className="sanciones-text" md={6}>
                  <p>SANCIONES REGISTRADAS</p>
                  <p>NOMBRES Y APELLIDOS: ALTAMIRANO GONZALES NATHALY DAYANA</p>
                  <p>Este Alumno no tiene Sanciones Registradas</p>
                 </Col>
-
             </Row>
         </div>
     )    
