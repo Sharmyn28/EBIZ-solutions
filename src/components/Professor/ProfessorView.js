580d7a52a4a6f52669e0ff9d83df154fa7dfe652
gabiprds93 <gabiprds.93@gmail.com>
2017-12-12 16:00:54 -0500

    Arreglo singout

--- src/components/Professor/ProfessorView.js
+++ src/components/Professor/ProfessorView.js
@@ -7,25 +7,29 @@ import { connect } from 'redux-zero/react';
 import { NavLink, Redirect } from 'react-router-dom';
 
 const Professor = ({ user, successLogin }) => {
-    console.log ( "successLogin",  successLogin)
-    return (
-        <Grid fluid>
-         {
-                !successLogin && <Redirect to="/home" />
-            }             
-          <Row>
-             <SideBar />
-             <Col lg={8} md={8}></Col>
-             <Col lg={8} md={8}>
-                 <TableInformation user={user}/>
-                 <TableMarks user={user} />
-                 <Col lgOffset={5} lg={2} md={2}><Button>Guardar Notas</Button></Col> 
-             </Col>
-         </Row>
+console.log ( "successLogin", successLogin)
+return (
+<Grid fluid>
+{
+!successLogin && <Redirect to="/home" />
+} 
+{
+successLogin && 
+<Row>
+<SideBar />
+<Col lg={8} md={8}></Col>
+<Col lg={8} md={8}>
+<TableInformation user={user}/>
+<TableMarks user={user} />
+<Col lgOffset={5} lg={2} md={2}><Button>Guardar Notas</Button></Col> 
+</Col>
+</Row>
+} 
 
-         }
-        </Grid>
-    )
+
+}
+</Grid>
+)
 }
 
 //export default Professor;
