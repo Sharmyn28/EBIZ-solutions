e63efad4220d1c5c097b14460b6ff19035c6f99c
evecarina <carina120794@gmail.com>
2017-12-11 23:05:23 -0500

    maquetando sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -7,23 +7,22 @@ const SideBar =()=>{
     <Col lg={3} md={3} xs={3} smHidden xsHidden>
         <Accordion className="container-panel">
         <Panel header="Matriculas" eventKey="1">
-           <NavLink to={"/simulation"}> Simulación de Matricula</NavLink>
+           <NavLink to={"/simulation"}>Simulación de Matricula</NavLink>
         </Panel>
         <Panel header="Inf.Académica" eventKey="2">          
             <NavLink to={"/professor"}><p>Consolidado de Notas</p></NavLink>
             <NavLink to={"/fileData"}>Ficha Académica</NavLink>      
-        </Panel>
-        <Panel header="Consultas" eventKey="3">
-        <NavLink to={"/samePage"}><p>Plan de Estudio</p></NavLink>
-        </Panel>
-        <Panel header="Información Personal" eventKey="4">        
-        </Panel>
-        <Panel header="Sanciones" eventKey="4">
+        </Panel>        
+        <Panel header="Sanciones" eventKey="3">
         <NavLink to={"/sanciones"}>Detalles</NavLink>
         </Panel>
         <Panel header="Preguntas Frecuentes" eventKey="4">        
         </Panel>
-        <Panel header="Salir" eventKey="5">
+        <Panel header="Consultas" eventKey="5">
+        </Panel>
+        <Panel header="Información Personal" eventKey="6">        
+        </Panel>
+        <Panel header="Salir" eventKey="7">
         </Panel>
         </Accordion>
     </Col>
