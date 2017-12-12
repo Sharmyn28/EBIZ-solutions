41bffea1ff21bf3e2d909a52a3a01677e21dd8c8
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-12-12 12:21:41 -0500

    comentar teachers

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,7 @@
 import createStore from 'redux-zero';
 import students from "./Students";
 import registrationCourses from "./RegistrationCourses";
-<<<<<<< HEAD
-//import teacher from "./Teachers";
-=======
 // import teacher from "./Teachers";
->>>>>>> e130fece4e79fd414c5f2a4227e94c84b69ac5b2
 import courses from "./Courses";
 import { GrandTotal } from '../actions/actions';
 
